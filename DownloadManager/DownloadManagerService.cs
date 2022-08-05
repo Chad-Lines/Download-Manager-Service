54ee5134157c3bb0c7fd593b7a76dc1a30bff45e
Chad <chad.lines1@gmail.com>
2022-08-05 01:31:59 -0600

    0.003 - Starting on the REAL Work

---
+++ DownloadManager/DownloadManagerService.cs
@@ -0,0 +1,19 @@
+﻿namespace DownloadManager;
+
+public class DownloadManagerService
+{
+    
+    public static void Main()
+    {
+        // Setting up the path
+        string _downloadPath = @"C:\Users\chadl\Downloads";
+
+        // Getting the contents of the folder
+        string[] _files = Directory.GetFiles(_downloadPath);
+
+        foreach (string f in _files)
+        {
+            Console.WriteLine(f);
+        }
+    }
+}
