6d72621ec3cdd3ea0ad300458ee0f20502ed2c7a
Chad <chad.lines1@gmail.com>
2022-08-05 01:16:02 -0600

    0.001 - Initialize Project

---
+++ DownloadManager/Program.cs
@@ -0,0 +1,10 @@
+using DownloadManager;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+await host.RunAsync();
