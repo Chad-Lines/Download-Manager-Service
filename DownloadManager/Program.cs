b1118da2a2a6e01a333905662b3cef6f222f3b1a
Chad <chad.lines1@gmail.com>
2022-08-06 09:52:29 -0600

    0.005 - Access Download Folder

---
+++ DownloadManager/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace DownloadManager;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        // Defining the source and destination directories
+        string sourceDir = @"C:\Users\chadl\Downloads";
+        string destDirDocuments = = @"C:\Users\chadl\Documents";
+        string destDirMusic = @"C:\Users\chadl\Music";
+        string destDirPictures = @"C:\Users\chadl\Pictures";
+        string destDirVideos = @"C:\Users\chadl\Videos";
+
+        string[] downloadFiles = Directory.GetFiles(sourceDir);
+
+        foreach (string f in downloadFiles) {  Console.WriteLine(f); }
+    }
+}
